c22e92ca523236fe0e728004933ea3c95b2af876
Baygid Hasan <baygid234@gmail.com>
2023-12-16 20:57:01 +0600

    add a new python file

---
+++ tryy.py
@@ -0,0 +1,13 @@
+print("i love money :")
+money = int(input())
+
+start = 1
+end = money
+
+while start <= end:
+    for value in range(start):
+        print("$", end="")
+
+        print()
+        start += 1
+
